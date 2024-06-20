adb03ef70f4853f56a5f04a93442480053fe8995
sarachafikidrissi <chafikidrissisara@gmail.com>
2024-06-20 16:59:55 +0100

    menu items created

--- foody/src/Pages/Home/Home.js
+++ foody/src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
+import ExploreMenu from '../../Components/ExploreMenu/ExploreMenu'
 
 function Home() {
   return (
     <div>
       <Header />
+      <ExploreMenu />
     </div>
   )
 }
